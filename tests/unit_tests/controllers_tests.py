ca8c1e9ec5104352859b5330db1a75dbc6b1e89a
Steve <mas.ste@gmail.com>
2024-08-02 17:29:31 +0200

    Fixed jwt auth

--- tests/unit_tests/controllers_tests.py
+++ tests/unit_tests/controllers_tests.py
@@ -1,6 +1,7 @@
 from sqlalchemy import inspect
 
-from epic_events.controllers.authentication import is_email_exists
+from epic_events.controllers.auth_controller import is_email_exists, register
+from epic_events.models.employee import Employee
 
 
 def test_db_connection(engine):
@@ -8,7 +9,7 @@ def test_db_connection(engine):
     assert inspector.engine.url.database == "test_epic_events"
 
 
-"""def test_register_employee(db):
+def test_register_employee(db):
     password_test = "qwertyui"
 
     register("john",
@@ -20,7 +21,7 @@ def test_db_connection(engine):
              db)
 
     assert db.query(Employee).filter_by(
-        email='test@epic-events.com').first() is not None"""
+        email='test@epic-events.com').first() is not None
 
 
 def test_is_email_exists():
